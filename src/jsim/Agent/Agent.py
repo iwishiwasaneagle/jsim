bd99a02173437c9f69d63e1eb49be486f9d19378
iwishiwasaneagle <jh.ewers@gmail.com>
2021-11-30 16:35:16 +0000

    Start of file structure

---
+++ src/jsim/Agent/Agent.py
@@ -0,0 +1,9 @@
+class Agent:
+    def __init__(self) -> None:
+        pass
+
+    def start_trial(self) -> None:
+        pass
+
+    def step(self) -> None:
+        pass
---
+++ src/jsim/Simulation/Simulation.py
@@ -0,0 +1,15 @@
+class Simulation:
+    def __init__(self) -> None:
+        pass
+
+    def start_trial(self) -> None:
+        pass
+
+    def steps(num_steps: int) -> None:
+        pass
+
+    def trials(num_trials: int, max_steps_per_trial) -> None:
+        pass
+
+    def collect_data(self) -> None:
+        pass
