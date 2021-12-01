095f8d438c607cedabf63aa6b1f90c1a336d73c2
iwishiwasaneagle <jh.ewers@gmail.com>
2021-12-01 11:49:57 +0000

    test: Test if the proper methods are abstract via sets

--- tests/Agent/test_Agent.py
+++ tests/Agent/test_Agent.py
@@ -1,5 +1,16 @@
+import pytest
+
 from jsim.Agent import Agent
 
 
-def test_Agent():
-    Agent()
+@pytest.fixture
+def agent():
+    Agent.__abstractmethods__ = set()
+    a = Agent()
+    yield a
+
+
+def test_Agent_is_properly_abstract():
+    abstract_methods = frozenset({"step", "start_trial", "__init__"})
+    assert Agent.__abstractmethods__ - abstract_methods == frozenset()
+    assert abstract_methods - Agent.__abstractmethods__ == frozenset()
