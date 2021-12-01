d36897a45e7763aeca9ede150b4f84f02992c020
iwishiwasaneagle <jh.ewers@gmail.com>
2021-12-01 12:13:43 +0000

    whitespace

--- release.sh
+++ release.sh
@@ -2,23 +2,23 @@
 
 # takes the tag as an argument (e.g. v0.1.0)
 if [ -n "$1" ]; then
-	git-cliff --tag "$1" > CHANGELOG.md
-	git commit CHANGELOG.md -m "chore(release): prepare for $1"
-	git show
-	# generate a changelog for the tag message
-	export TEMPLATE="\
-	{% for group, commits in commits | group_by(attribute=\"group\") %}
-	{{ group | upper_first }}\
-	{% for commit in commits %}
-		- {{ commit.message | upper_first }} ({{ commit.id | truncate(length=7, end=\"\") }})\
-	{% endfor %}
-	{% endfor %}"
-	changelog=$(git-cliff --unreleased --strip all)
-	# create a signed tag
-	# https://keyserver.ubuntu.com/pks/lookup?search=0x4A92FA17B6619297&op=vindex
-	git tag -s -a "$1" -m "Release $1" -m "$changelog"
-	git tag -v "$1"
+    git-cliff --tag "$1" > CHANGELOG.md
+    git commit CHANGELOG.md -m "chore(release): prepare for $1"
+    git show
+    # generate a changelog for the tag message
+    export TEMPLATE="\
+    {% for group, commits in commits | group_by(attribute=\"group\") %}
+    {{ group | upper_first }}\
+    {% for commit in commits %}
+    	- {{ commit.message | upper_first }} ({{ commit.id | truncate(length=7, end=\"\") }})\
+    {% endfor %}
+    {% endfor %}"
+    changelog=$(git-cliff --unreleased --strip all)
+    # create a signed tag
+    # https://keyserver.ubuntu.com/pks/lookup?search=0x4A92FA17B6619297&op=vindex
+    git tag -s -a "$1" -m "Release $1" -m "$changelog"
+    git tag -v "$1"
     git push origin "$1"
 else
-	echo "warn: please provide a tag"
+    echo "warn: please provide a tag"
 fi
