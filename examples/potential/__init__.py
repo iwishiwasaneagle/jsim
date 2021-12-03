31b8d4354a92a71fb453c4b9195790c8e018e7ad
iwishiwasaneagle <jh.ewers@gmail.com>
2021-12-03 17:53:02 +0000

    move examples about to integrate into docs better

---
+++ examples/potential/__init__.py
@@ -0,0 +1,12 @@
+from .potential_based_nav import (
+    Acceleration,
+    Force,
+    PhysicalState,
+    Position,
+    PotentialAgent,
+    PotentialEnv,
+    PotentialSim,
+    Velocity,
+    XYState,
+    main,
+)
